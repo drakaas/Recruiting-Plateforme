fbbb5937017aa07371eb0e69fae171e7bc7ea14d
Drakaas <oussama.almamoun@ensiie.eu>
2025-11-07 02:40:56 +0200

    d

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://35.180.152.70:4000/api'
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://35.180.152.70/api'
 
 
